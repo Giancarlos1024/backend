e8c1be453d7a4c4aa3d012a130f3c834b286254c
Yasashi-OF <orlandofabrisiomh@gmail.com>
2025-01-12 22:12:33 -0500

    cambios

--- Controllers/Controllers_ComisionesTemporales.js
+++ Controllers/Controllers_ComisionesTemporales.js
@@ -30,9 +30,47 @@ const saveComisionTemporal = (req, res) => {
     })
 }
 
+//Actualizar algun registro
+const updateComisionTemporal = (req, res) => {
+    const { id } = req.body;
+    const { poliza, n_factura, fecha, ramo_asegurador, ramo_sbs, moneda, monto_prima, monto_comision, nombre_usuario } = req.body;
+
+    const query = `
+        UPDATE comision_temporal SET
+            poliza = ?, n_factura = ?, fecha = ?, ramo_asegurador = ?, ramo_sbs = ?, moneda = ?, monto_prima = ?, monto_comision = ?, nombre_usuario = ?
+        WHERE id = ?
+    `;
+
+    const values = [poliza, n_factura, fecha, ramo_asegurador, ramo_sbs, moneda, monto_prima, monto_comision, nombre_usuario, id];
+
+    pool.query(query, values, (err, results) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.status(200).json({ message: 'Comisión temporal actualizada exitosamente' });
+    });
+}
+
+//Eliminar registro
+const deleteComisionTemporal = (req, res) => {
+    const { id } = req.body;
+
+    const query = 'DELETE FROM comision_temporal WHERE id = ?';
+
+    pool.query(query, [id], (err, results) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.status(200).json({ message: 'Comision temporal eliminada exitosamente' });
+    });
+};
 
 module.exports = {
     getComisionTemporalDatos,
     saveComisionTemporal,
+    updateComisionTemporal,
+    deleteComisionTemporal
 }
 
--- Routes/RouterComisionesTemporales.js
+++ Routes/RouterComisionesTemporales.js
@@ -4,10 +4,14 @@ const router = express.Router();
 const{
     getComisionTemporalDatos,
     saveComisionTemporal,
+    updateComisionTemporal,
+    deleteComisionTemporal,
 } = require('../Controllers/Controllers_ComisionesTemporales')
 
 //Rutas
 router.get('/comisiontemp', getComisionTemporalDatos);
 router.post('/guardarcomision', saveComisionTemporal);
+router.put('/actualizarcomision', updateComisionTemporal);
+router.delete('/borrarcomision', deleteComisionTemporal);
 
 module.exports = router;
